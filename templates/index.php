5d049aacda747db430a431df11e5a0010a725890
farkas <farkas@farkas-Lenovo-3000-N500>
2013-04-19 01:26:54 +0200

    base application finished

--- templates/index.php
+++ templates/index.php
@@ -1,14 +1,29 @@
 <script type="text/javascript">
+function getAddForm(){
+   event.preventDefault();
+//   $('td.ajax').toggle("slow");
+   $.ajax({
+      type: "GET",
+      url: "some.php",
+      data: { name: "John", location: "Boston" },
+      success: function(data){
+        $('td.ajax').toggle("slow");
+        $('td.ajax').html(data);
+     }
+   });   
+}    
+
 $(document).ready(function() {
-   $('td.ajax').load('templates/addForm.php');
+    
+   $('td.ajax').css('display','hide');
+   $('td.ajax').hide();
  });    
 </script>
-
 <a href="logout.php">Kijelentkezés(<?php echo $_SESSION['uname']; ?>)</a>
 <h1>Főoldal</h1>
 <br />            
 <br />
-<a href="index.php?page=add">Új névjegy hozzáadása</a>
+<a href="index.php?page=add" id="add" name="add" onClick="getAddForm()">Új névjegy hozzáadása</a>
 
 <?php 
 function decorate($str){
@@ -38,7 +53,7 @@ echo '</td>';
 
 ?>
 <td class="ajax">
-    
+    ef
 </td>
 </tr>
 </table>
